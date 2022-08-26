dd712196489625088fcaa2a2ea48eb64999d2d96
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-26 17:57:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    //check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    // if any letters dont match, return false
+    if (startChar !== endChar) return false;
+
+  }
+  // return value
+  return true;
 }
+
+
+
 
 /* 
   Add your pseudocode here
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
 */
 
 /*
